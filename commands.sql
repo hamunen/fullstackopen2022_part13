0c58e81658fd59fea1201b2f658c40d64181f7e5
Henri <henri.hamunen@gmail.com>
2023-04-08 19:52:04 +0300

    Task 13.2.

---
+++ commands.sql
@@ -0,0 +1,12 @@
+CREATE TABLE blogs (
+    id SERIAL PRIMARY KEY,
+    author text,
+    url text NOT NULL,
+    title text NOT NULL,
+    likes integer NOT NULL DEFAULT 0
+);
+
+INSERT INTO blogs (author, url, title, likes) VALUES ('Gill Bates', 'microsoft.com', 'Hey guys whatsup', 6790);
+INSERT INTO blogs (author, url, title, likes) VALUES ('Henri Hamunen', 'myspace.com/coolguy', 'My blog', 12);
+
+select * from blogs;

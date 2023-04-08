3fa2db6e455ce9b7068a456a63b2e34a14ba52f7
Henri <henri.hamunen@gmail.com>
2023-04-08 22:21:57 +0300

    Task 13.7. error handling

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -23,33 +23,35 @@ router.get('/:id', blogFinder, async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-  try {
-    const blog = await Blog.create(req.body)
-    return res.json(blog)
-  } catch (error) {
-    return res.status(400).json({ error })
-  }
+  const blog = await Blog.create(req.body)
+  return res.json(blog)
 })
 
 router.put('/:id', blogFinder, async (req, res) => {
-  if (req.blog) {
-    req.blog.likes = req.body.likes
-    await req.blog.save()
-    res.json(req.blog)
-  } else {
-    res.status(404).end()
-  }
+  if (!req.blog) return res.status(404).end()
+  if (!req.body.likes || typeof req.body.likes !== 'number')
+    throw new Error('Invalid or missing likes, idiot')
+  req.blog.likes = req.body.likes
+  await req.blog.save()
+  res.json(req.blog)
 })
 
 router.delete('/:id', blogFinder, async (req, res) => {
-  try {
-    if (req.blog) {
-      await req.blog.destroy()
-    }
-    res.status(204).end()
-  } catch (error) {
-    return res.status(400).json({ error })
+  if (req.blog) {
+    await req.blog.destroy()
   }
+  res.status(204).end()
 })
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.name, error.message)
+
+  // if (error.name == 'SequelizeDatabaseError')
+  return res.status(400).json({ error: error.message })
+
+  //next(error)
+}
+
+router.use(errorHandler)
 
 module.exports = router

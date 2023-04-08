6d1bd3afad4860e82f4d288200030b1cf701eb6b
Henri <henri.hamunen@gmail.com>
2023-04-08 21:38:21 +0300

    Task 13.6. add put

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -31,6 +31,16 @@ router.post('/', async (req, res) => {
   }
 })
 
+router.put('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    req.blog.likes = req.body.likes
+    await req.blog.save()
+    res.json(req.blog)
+  } else {
+    res.status(404).end()
+  }
+})
+
 router.delete('/:id', blogFinder, async (req, res) => {
   try {
     if (req.blog) {

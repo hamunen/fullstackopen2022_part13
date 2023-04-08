e8aa2ae40c70ee93247289ef5b5f0148c2725d75
Henri <henri.hamunen@gmail.com>
2023-04-08 21:34:28 +0300

    Task 13.5. restructure

---
+++ controllers/blogs.js
@@ -0,0 +1,45 @@
+const router = require('express').Router()
+
+const { Blog } = require('../models')
+
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id)
+  next()
+}
+
+router.get('/', async (req, res) => {
+  const blogs = await Blog.findAll()
+  console.log(JSON.stringify(blogs, null, 2))
+  res.json(blogs)
+})
+
+router.get('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    console.log(req.blog.toJSON())
+    res.json(req.blog)
+  } else {
+    res.status(404).end()
+  }
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body)
+    return res.json(blog)
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
+})
+
+router.delete('/:id', blogFinder, async (req, res) => {
+  try {
+    if (req.blog) {
+      await req.blog.destroy()
+    }
+    res.status(204).end()
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
+})
+
+module.exports = router

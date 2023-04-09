9d02a753a649fee2efd4a8193ea247f0517a0bb4
Henri <henri.hamunen@gmail.com>
2023-04-09 14:06:28 +0300

    Task 13.8. users

---
+++ controllers/users.js
@@ -0,0 +1,47 @@
+const router = require('express').Router()
+
+const { User } = require('../models')
+
+router.get('/', async (req, res) => {
+  const users = await User.findAll()
+  res.json(users)
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const user = await User.create(req.body)
+    res.json(user)
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
+})
+
+router.put('/:username', async (req, res) => {
+  const user = await User.findOne({ where: { username: req.params.username } })
+  if (!user) return res.status(404).end()
+  if (!req.body.username || typeof req.body.username !== 'string')
+    throw new Error('Invalid or missing username, idiot')
+
+  user.username = req.body.username
+  await user.save()
+  res.json(user)
+})
+
+router.get('/:id', async (req, res) => {
+  const user = await User.findByPk(req.params.id)
+  if (user) {
+    res.json(user)
+  } else {
+    res.status(404).end()
+  }
+})
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.name, error.message)
+
+  return res.status(400).json({ error: error.message })
+}
+
+router.use(errorHandler)
+
+module.exports = router

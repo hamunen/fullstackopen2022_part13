6beccd383a82c2d239633e02c930842dac8c86ec
Henri <henri.hamunen@gmail.com>
2023-04-09 15:11:03 +0300

    Task 13.16. authors group by

---
+++ controllers/authors.js
@@ -0,0 +1,29 @@
+const router = require('express').Router()
+const { Op } = require('sequelize')
+
+const { Blog } = require('../models')
+const { sequelize } = require('../util/db')
+
+router.get('/', async (req, res) => {
+  const authors = await Blog.findAll({
+    group: ['author'],
+    attributes: [
+      'author',
+      [sequelize.fn('COUNT', sequelize.col('id')), 'articles'],
+      [sequelize.fn('SUM', sequelize.col('likes')), 'likes'],
+    ],
+  })
+
+  console.log(JSON.stringify(authors, null, 2))
+  return res.json(authors)
+})
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.name, error.message)
+
+  return res.status(400).json({ error: error.message })
+}
+
+router.use(errorHandler)
+
+module.exports = router

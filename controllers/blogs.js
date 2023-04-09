079558412bad33c370ee162823f853fe6ae3875a
Henri <henri.hamunen@gmail.com>
2023-04-09 14:25:33 +0300

    Exercise 13.10. user linked to blg

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,9 +1,25 @@
 const router = require('express').Router()
+const { SECRET } = require('../util/config')
+const jwt = require('jsonwebtoken')
 
-const { Blog } = require('../models')
+const { Blog, User } = require('../models')
 
 const blogFinder = async (req, res, next) => {
   req.blog = await Blog.findByPk(req.params.id)
+  next()
+}
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch {
+      return res.status(401).json({ error: 'token invalid' })
+    }
+  } else {
+    return res.status(401).json({ error: 'token missing' })
+  }
   next()
 }
 
@@ -22,8 +38,9 @@ router.get('/:id', blogFinder, async (req, res) => {
   }
 })
 
-router.post('/', async (req, res) => {
-  const blog = await Blog.create(req.body)
+router.post('/', tokenExtractor, async (req, res) => {
+  const user = await User.findByPk(req.decodedToken.id)
+  const blog = await Blog.create({ ...req.body, userId: user.id })
   return res.json(blog)
 })
 
